e0697a96a5df5f1c41ac1e1bb4aa25a825335bfb
NOLOGY\Craig <craig@nology.io>
2018-11-27 14:44:37 +0000

    Added firebase api to prod

---
+++ src/environments/environment.prod.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = {
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyCVCABsn3G0zPqvbm9tfj9O9tH78rP5ObM',
+        authDomain: 'my-northstar.firebaseapp.com',
+        databaseURL: 'https://my-northstar.firebaseio.com',
+        projectId: 'my-northstar',
+        storageBucket: 'my-northstar.appspot.com',
+        messagingSenderId: '540559413927'
+    }
+};
