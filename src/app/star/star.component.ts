ce1d591a0b5069f02f33141cd8fb4f5b41926587
Georgina Ryder <Georgina.Ryder@nology.io>
2018-11-27 12:05:20 +0000

    star added in results page

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./star.component.scss']
 })
 export class StarComponent implements OnInit {
+  categories = [
+    {
+      category: "Happy",
+      score: 5,
+    },
+    {
+      category: "Finances",
+      score: 8,  
+    },
+    {
+      category: "Fun & Recreation",
+      score: 3,  
+    },
+    {
+      category: "Relationships",
+      score: 4  
+    }
+
+  ]
+
 
   constructor() { }
 
