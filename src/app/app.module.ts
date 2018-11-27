5d074294fc35eaa7d81436c9a78392c24bc3e998
Satyam Gehani <Satyam.Gehani@nology.io>
2018-11-27 12:14:46 +0000

    created task list and create task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ResultsComponent } from './results/results.component';
 import { StarComponent } from './star/star.component';
 import { TaskManagerComponent } from './task-manager/task-manager.component';
 import { LayoutAppComponent } from './layout-app/layout-app.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 const routes: Routes = [
@@ -34,7 +36,9 @@ const routes: Routes = [
     ResultsComponent,
     StarComponent,
     TaskManagerComponent,
-    LayoutAppComponent
+    LayoutAppComponent,
+    CreateTaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
