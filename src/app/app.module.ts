31efc1c9a3843ef8720d2246d7b2080ad42927bc
Shea Murphy <Shea.Murphy@nology.io>
2018-11-27 10:56:35 +0000

    created layout-auth component + register & forgot password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutAuthComponent } from './layout-auth/layout-auth.component';
+import { RegisterComponent } from './register/register.component';
+import { FPasswordComponent } from './f-password/f-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutAuthComponent,
+    RegisterComponent,
+    FPasswordComponent
   ],
   imports: [
     BrowserModule
