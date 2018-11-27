aed0898bb1ce4b47b1fad5c2a38697877b4f3139
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2018-11-27 11:46:11 +0000

    questionnaire form done

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IQuestion {
+  title:string,
+  number:number,
+  content:string,
+  score:number,
+  weighting:number,
+  category:string
+}
+
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.component.html',
@@ -9,26 +18,43 @@ export class QuestionnaireComponent implements OnInit {
   questions = [ 
     {
     title: 'Question 1',
+    number: 1,
     content: 'I do not feel particularly pleased with the way I am',
-    weighting: 2
+    score: undefined,
+    weighting: 2,
+    category: 'Happiness'
     },
     {
     title: 'Question 2',
+    number: 2,
     content: 'I feel that life is very rewarding',
-    weighting: 5
+    score: undefined,
+    weighting: 5,
+    category: 'Happiness'
     },
     {
     title: 'Question 3',
+    number: 3,
     content: 'I rarely wake up feeling rested',
-    weighting: 5
+    score: undefined,
+    weighting: 5,
+    category: 'Happiness'
     },
+    {
+    title: 'Question 4',
+    number: 4,
+    content: 'I laugh a lot',
+    score: undefined,
+    weighting: 2,
+    category: 'Happiness'
+    }
     ]
 
   constructor() {
+   }  
 
-   }
-
-  ngOnInit() {
+   ngOnInit() {
+  
   }
 
 }
