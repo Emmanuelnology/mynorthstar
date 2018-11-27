027973ec5fb12943834d5b3101bab3b11ed7fba9
Caitlin Whiteley <Caitlin.Whiteley@nology.io>
2018-11-27 11:04:25 +0000

    Example questions

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionnaireService, IQuestion, IResult } from '../services/questionnaire.service';
+
 
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.component.html',
   styleUrls: ['./questionnaire.component.scss']
 })
-export class QuestionnaireComponent implements OnInit {
 
-  constructor() { }
+export class QuestionnaireComponent implements OnInit {
+  questions:IQuestion[] = [
+    {
+      score: 5,
+      weight: 2,
+      category: 'friends'
+    },
+    {
+      score: 1,
+      weight: 4,
+      category: 'finance'
+    },
+    {
+      score: 5,
+      weight: 5,
+      category: 'friends'
+    },
+    {
+      score: 9,
+      weight: 2,
+      category: 'finance'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/services/questionnaire.service.ts
+++ src/app/services/questionnaire.service.ts
@@ -1,10 +1,66 @@
 import { Injectable } from '@angular/core';
 
+interface CategoryObjects {
+  score:number;
+  weight:number;
+}
+
+interface Result {
+  category:string;
+  categoryAverage:number;
+}
+
+interface QuestionnaireObject {
+  score:number;
+  weight:number;
+  category:string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class QuestionnaireService {
-
+  
   constructor() { }
+
+  calculateWeightedAverage(array:CategoryObjects[]):number {
+    let weightTimesScoreSum = 0;
+    let weightSum = 0;
+    for (let index in array) {
+      weightTimesScoreSum += array[index].score*array[index].weight;
+      weightSum += array[index].weight;
+    }
+    let averageScore = weightTimesScoreSum/weightSum;
+    return averageScore;
+  }
+  
+  getResults(initialResults:QuestionnaireObject[]):Result[] {
+    let friendsArray:CategoryObjects[];
+    let financeArray:CategoryObjects[];
+    
+    for (let index in initialResults) {
+      if (initialResults[index].category == 'friends') {
+        let catOb:CategoryObjects;
+        catOb.score = initialResults[index].score;
+        catOb.weight = initialResults[index].weight;
+        friendsArray.push(catOb);
+      } else if (initialResults[index].category == 'finance') {
+        let catOb:CategoryObjects;
+        catOb.score = initialResults[index].score;
+        catOb.weight = initialResults[index].weight;
+        financeArray.push(catOb);
+      }
+    }
+    let friendsAverage = {
+      category:'friends', 
+      categoryAverage: this.calculateWeightedAverage(friendsArray)
+    }
+    let financeAverage = {
+      category: 'finance', 
+      categoryAverage: this.calculateWeightedAverage(financeArray)
+    }
+    let results = [friendsAverage, financeAverage];
+    return results;
+  }
 }
