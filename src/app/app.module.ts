0484e1ad56d1665b6d5a5227ad7ead176b12cb08
Georgina Ryder <Georgina.Ryder@nology.io>
2018-11-27 10:41:15 +0000

    results page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
+import { StarComponent } from './results/star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule
