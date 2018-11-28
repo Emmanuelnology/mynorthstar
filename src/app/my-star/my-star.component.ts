81ff89b9fce2c34fd38a46eab9c5259f041c713a
NOLOGY\Craig <craig@nology.io>
2018-11-28 14:07:39 +0000

    Refactored chart code

--- src/app/my-star/my-star.component.ts
+++ src/app/my-star/my-star.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IResult, QuestionnaireService, exampleQuestions } from '../services/questionnaire.service';
+import { IData } from '../star/star.component';
 
 @Component({
   selector: 'app-my-star',
@@ -8,16 +9,70 @@ import { IResult, QuestionnaireService, exampleQuestions } from '../services/que
 })
 export class MyStarComponent implements OnInit {
   questions = exampleQuestions;
-  results: IResult[];
+  results: IResult[] = [];
   overallResult: number;
+  starData: IData = {
+    datasets: [],
+    labels: [],
+    options: {
+      legend: {
+        display: false
+      },
+      scale: {
+        pointLabels: {
+          fontColor: 'white',
+          fontSize: 14
+        },
+        angleLines: {
+          color: '#b02062'
+        },
+        ticks: {
+          display: false,
+          min: 0,
+          max: 10,
+        },
+        gridLines: {
+          color: '#777'
+        }
+      }
+    }
+  };
 
   constructor(private questionnaireService: QuestionnaireService) {
     this.results = this.questionnaireService.getResults(this.questions);
     this.overallResult = this.questionnaireService.overallAverage(this.results);
+
+    const restructuredData = this.restructureData(this.results);
+    this.starData .datasets = restructuredData.datasets;
+    this.starData .labels = restructuredData.labels;
+  }
+
+  restructureData(results) {
+    const categories: string[] = [];
+    const data: number[] = [];
+    for (const result of results) {
+      categories.push(result.category);
+      data.push(result.categoryAverage);
+    }
+    return {
+      datasets: [
+        {
+          data: data,
+          label: '',
+          fill: false,
+          lineTension: 0.3,
+          borderColor: 'white',
+          pointBorderColor: '#6ecbd3',
+          pointRadius: 5,
+          pointBackgroundColor: '#37234f'
+        }
+      ],
+      labels: categories
+    };
+
   }
 
   ngOnInit() {
-    this.overallResult = 7;
   }
 
 }
--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,7 +1,5 @@
-import { Component, Input, AfterViewInit } from '@angular/core';
-import {  IResult } from '../services/questionnaire.service';
+import { Component, Input, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { Chart } from 'chart.js';
-import {ViewChild, ElementRef} from '@angular/core';
 
 
 interface ICanvas extends HTMLElement {
@@ -31,8 +29,7 @@ interface IRadarChartOptions {
   };
 }
 
-interface IChartDataSetOptions {
-
+interface IChartDataSet {
   data: number[];
   label: string;
   fill: boolean;
@@ -41,7 +38,12 @@ interface IChartDataSetOptions {
   pointBorderColor: string;
   pointRadius: number;
   pointBackgroundColor: string | string[];
+}
 
+export interface IData {
+  datasets: IChartDataSet[];
+  labels: string[];
+  options: IRadarChartOptions;
 }
 
 @Component({
@@ -49,83 +51,30 @@ interface IChartDataSetOptions {
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent implements AfterViewInit {
-  @Input() results: IResult[];
 
-  @ViewChild('radarChart') canvas: ElementRef;
+export class StarComponent implements AfterViewInit {
+  @Input() data: IData;
   chart = [];
 
-  public radarChartLabels: string[] = [];
-  public radarChartOptions: IRadarChartOptions = {
-    legend: {
-      display: false
-    },
-    scale: {
-      pointLabels: {
-        fontColor: 'white',
-        fontSize: 14
-      },
-      angleLines: {
-        color: '#b02062'
-      },
-      ticks: {
-        display: false,
-        min: 0,
-        max: 10,
-      },
-      gridLines: {
-        color: '#777'
-      }
+  constructor(private cd: ChangeDetectorRef){}
 
-    },  };
-    public radarChartData: IChartDataSetOptions[] = [];
-
-
-    constructor() {
-
-    }
-
-
-    createChart() {
+  createChart() {
       const canvas: ICanvas = (document.getElementById('canvas') as ICanvas);
       const ctx = canvas.getContext('2d');
       this.chart = new Chart(ctx, {
         type: 'radar',
         data: {
-          labels: this.radarChartLabels,
-          datasets: this.radarChartData
+          labels: this.data.labels,
+          datasets: this.data.datasets,
         },
-        options: this.radarChartOptions
+        options: this.data.options,
       });
 
     }
 
-    restructureData() {
-      const categories: string[] = [];
-      const data: number[] = [];
-      for (const result of this.results) {
-        categories.push(result.category);
-        data.push(result.categoryAverage);
-      }
-      this.radarChartData = [
-        {
-          data: data,
-          label: '',
-          fill: false,
-          lineTension: 0.3,
-          borderColor: 'white',
-          pointBorderColor: '#6ecbd3',
-          pointRadius: 5,
-          pointBackgroundColor: '#37234f'
-        }
-      ];
-      this.radarChartLabels = categories;
-    }
-
-
     ngAfterViewInit() {
-      this.restructureData();
       this.createChart();
+      this.cd.detectChanges();
     }
 
   }
