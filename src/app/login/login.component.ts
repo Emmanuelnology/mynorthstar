b179b066a9b2c47c8facc71c2bd228305784fe0d
Luke Delaney <Luke.Delaney@nology.io>
2018-11-30 09:37:24 +0000

    forgot password adjusted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  showPassword = false;
+  showPassword = true;
+
+  togglePassword(){
+    this.showPassword=!this.showPassword
+  }
   constructor() { }
 
   ngOnInit() {
