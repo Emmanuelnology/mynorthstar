37eb8ff5cc7d541e3b8fb19db906476a6194a446
George <gcwebster@hotmail.co.uk>
2018-12-20 14:15:44 +0000

    improvements to file

--- src/app/user-profile-options/user-profile-options.component.ts
+++ src/app/user-profile-options/user-profile-options.component.ts
@@ -71,8 +71,8 @@ export class UserProfileOptionsComponent implements OnInit {
   }
 
   updateClientImage() {
-    //If statement will only execute update method if it is a valid URL.
-    if(this.newImage.indexOf('http') == 0){
+    // If statement will only execute update method if it is a valid URL.
+    if (this.newImage.indexOf('http') === 0) {
       this.authService.changeImage(this.newImage).then(
         () => {
           this.newImage = '';
@@ -81,15 +81,12 @@ export class UserProfileOptionsComponent implements OnInit {
       )
       .catch(
         (error) => {
-          if(error.message == '{"error":{"code":400,"message":"INVALID_PROFILE_ATTRIBUTE","errors":[{"message":"INVALID_PROFILE_ATTRIBUTE","domain":"global","reason":"invalid"}]}}')
-            this.errorNewImage = "Please make sure this is a valid URL, it must begin with 'http://' or 'https://' ";
-          else
-            this.errorNewImage = error.message;
+          this.errorNewImage = error.message;
         }
       );
+    } else {
+      this.errorNewImage = 'Cannot update image, it must be a URL (beginning with \'http://\' or \'https://\'';
     }
-    else
-      this.errorNewImage = "Cannot update image, it must be a URL (beginning with 'http://' or 'https://'";
   }
 
   updateClientPassword() {
